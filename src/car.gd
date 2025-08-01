c391e440e14b66f085dc1c863c1ef8fec2b022c8
NicoheroesDX <nico@schulten-online.de>
2025-08-01 23:27:10 +0200

    Improved vehicle behaviour and added car > road_type relations

--- src/car.gd
+++ src/car.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D;
 
 class_name Car;
 
+var map: TileMap;
+
 var current_speed: float = 0.0;
 
 const HANDLING: float = 60;
@@ -9,23 +11,51 @@ const ACCELERATION: float = 30;
 const DECELERATION: float = 10;
 const RESITANCE: float = 0.90;
 
+var steering_effect: float = 1.0;
+var speed_effect: float = 1.0;
+
 func _physics_process(delta: float) -> void:
+	apply_effects_for_road_type(get_current_road_type());
+	
 	var input_direction: float = Input.get_axis("car_steer_left", "car_steer_right");
-	const turn_power: float = HANDLING / 200000;
+	var turn_power: float = HANDLING / 200000;
 	
 	if abs(velocity.x) > abs(velocity.y):
 		current_speed = abs(velocity.x)
 	else:
 		current_speed = abs(velocity.y)
 	
-	rotation += turn_power * current_speed * input_direction;
+	rotation += turn_power * current_speed * input_direction * steering_effect;
 	
 	var direction = Vector2.UP.rotated(rotation);
 	
 	if Input.is_action_pressed("car_accelerate"):
-		velocity += direction * ACCELERATION;
+		velocity += direction * ACCELERATION * speed_effect;
 	if Input.is_action_pressed("car_reverse"):
-		velocity -= direction * DECELERATION;
+		velocity -= direction * DECELERATION * speed_effect;
+	
+	print(speed_effect);
 	
 	velocity *= RESITANCE;
 	move_and_slide();
+
+func connect_to_map(tile_map: TileMap) -> void:
+	map = tile_map;
+
+func get_current_road_type() -> String:
+	var current_tile = map.local_to_map(map.to_local(global_position))
+	var tile_data = map.get_cell_tile_data(-1, current_tile)
+	
+	if tile_data:
+		if tile_data.has_custom_data("RoadType"):
+			return tile_data.get_custom_data("RoadType")
+	return "";
+
+func apply_effects_for_road_type(road_type: String):	
+	match road_type:
+		"grass":
+			speed_effect = 0.3;
+			steering_effect = 0.8;
+		"asphalt", "finish", _:
+			speed_effect = 1.0;
+			steering_effect = 1.0;
--- src/world.gd
+++ src/world.gd
@@ -2,6 +2,13 @@ extends Node2D
 
 @onready var player_camera: Camera2D = $PlayerCamera;
 @onready var player: Car = $Player;
+@onready var tile_map: TileMap = $TileMap;
+
+func _ready() -> void:
+	player.connect_to_map(tile_map);
 
 func _process(delta: float) -> void:
+	update_camera_position();
+
+func update_camera_position():
 	player_camera.global_position = player.global_position;
