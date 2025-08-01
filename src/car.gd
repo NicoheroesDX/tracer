e4ff1e88b67ca86dfae6b188d1b365a07de1a250
NicoheroesDX <nico@schulten-online.de>
2025-08-01 21:10:54 +0200

    Added Basic Driving Physics

--- src/car.gd
+++ src/car.gd
@@ -1,3 +1,31 @@
-extends RigidBody2D
+extends CharacterBody2D;
 
 class_name Car;
+
+var current_speed: float = 0.0;
+
+const HANDLING: float = 60;
+const ACCELERATION: float = 30;
+const DECELERATION: float = 10;
+const RESITANCE: float = 0.90;
+
+func _physics_process(delta: float) -> void:
+	var input_direction: float = Input.get_axis("car_steer_left", "car_steer_right");
+	const turn_power: float = HANDLING / 200000;
+	
+	if abs(velocity.x) > abs(velocity.y):
+		current_speed = abs(velocity.x)
+	else:
+		current_speed = abs(velocity.y)
+	
+	rotation += turn_power * current_speed * input_direction;
+	
+	var direction = Vector2.UP.rotated(rotation);
+	
+	if Input.is_action_pressed("car_accelerate"):
+		velocity += direction * ACCELERATION;
+	if Input.is_action_pressed("car_reverse"):
+		velocity -= direction * DECELERATION;
+	
+	velocity *= RESITANCE;
+	move_and_slide();
