ddfbe3a2b0710017a86d55add776bb3bf917212b
NicoheroesDX <nico@schulten-online.de>
2025-08-29 21:41:07 +0200

    Changed the JS function - Deployed to test a ZIP download

--- src/global/webOnly.gd
+++ src/global/webOnly.gd
@@ -2,18 +2,19 @@ class_name WebOnly;
 extends Node;
 
 const JS_FUNCTION_DOWNLOAD_ZIP_FILE = """
-	window.godotFunctionDownloadZipFile = function(fileName, bytes) {
-		const blob = new Blob([bytes], { type: 'application/zip' });
+	(function() {
+		const bytes = Uint8Array.from(atob('%s'), c => c.charCodeAt(0));
+		const blob = new Blob([bytes], {type: 'application/zip'});
 		const a = document.createElement('a');
 		a.href = URL.createObjectURL(blob);
-		a.download = fileName;
+		a.download = '%s';
 		a.click();
 		URL.revokeObjectURL(a.href);
-	};
+	})();
 """
 
-static func initialize_java_script_functions():
-	JavaScriptBridge.eval(JS_FUNCTION_DOWNLOAD_ZIP_FILE);
-
 static func download_file(file_name: String, file_content: PackedByteArray):
-	JavaScriptBridge.call("godotFunctionDownloadZipFile", file_name, file_content);
+	var sanitized_file_name = file_name.replace("'", "\\'")
+	var base64_file_content = Marshalls.raw_to_base64(file_content).replace("\n", "");
+	
+	JavaScriptBridge.call(JS_FUNCTION_DOWNLOAD_ZIP_FILE.format(sanitized_file_name, base64_file_content));
