6df3a0b7a126869106d76a3302829a727954ae50
NicoheroesDX <nico@schulten-online.de>
2025-08-29 22:34:41 +0200

    Improved code

--- src/global/webOnly.gd
+++ src/global/webOnly.gd
@@ -1,23 +1,26 @@
 class_name WebOnly;
 extends Node;
 
-const JS_FUNCTION_DOWNLOAD_ZIP_FILE = """
-	const bytes = new Uint8Array([%s]);
-	const blob = new Blob([bytes], {type: 'application/zip'});
-	const a = document.createElement('a');
-	a.href = URL.createObjectURL(blob);
-	a.download = '%s';
-	a.click();
-	URL.revokeObjectURL(a.href);
-"""
-
 static func download_file(file_name: String, file_content: PackedByteArray):
 	var sanitized_file_name = file_name.replace("'", "\\'")
-	var u_int_8_array_string = "";
+	var file_content_as_string = concat_to_string(file_content);
 	
-	for i in file_content.size():
-		u_int_8_array_string += str(file_content[i]);
-		if (i < file_content.size() - 1):
-			u_int_8_array_string += ",";
+	JavaScriptBridge.eval("""
+		const bytes = new Uint8Array([""" + file_content_as_string + """]);
+		const blob = new Blob([bytes], {type: 'application/zip'});
+		const a = document.createElement('a');
+		a.href = URL.createObjectURL(blob);
+		a.download = '""" + sanitized_file_name + """';
+		a.click();
+		URL.revokeObjectURL(a.href);
+	""");
+
+static func concat_to_string(byte_array: PackedByteArray):
+	var result = "";
 	
-	JavaScriptBridge.eval(JS_FUNCTION_DOWNLOAD_ZIP_FILE.format([u_int_8_array_string, sanitized_file_name]));
+	for i in byte_array.size():
+		result += str(byte_array[i]);
+		if (i < byte_array.size() - 1):
+			result += ",";
+	
+	return result;
