719ab2b68276dec3e7833cff69e996cae55751a2
NicoheroesDX <nico@schulten-online.de>
2025-08-29 22:22:47 +0200

    Changed the JS function - Deployed to test a ZIP download

--- src/global/webOnly.gd
+++ src/global/webOnly.gd
@@ -1,6 +1,16 @@
 class_name WebOnly;
 extends Node;
 
+const JS_FUNCTION_DOWNLOAD_ZIP_FILE = """
+	const bytes = new Uint8Array([%s]);
+	const blob = new Blob([bytes], {type: 'application/zip'});
+	const a = document.createElement('a');
+	a.href = URL.createObjectURL(blob);
+	a.download = '%s';
+	a.click();
+	URL.revokeObjectURL(a.href);
+"""
+
 static func download_file(file_name: String, file_content: PackedByteArray):
 	var sanitized_file_name = file_name.replace("'", "\\'")
 	var u_int_8_array_string = "";
@@ -10,12 +20,4 @@ static func download_file(file_name: String, file_content: PackedByteArray):
 		if (i < file_content.size() - 1):
 			u_int_8_array_string += ",";
 	
-	JavaScriptBridge.eval("""
-		const bytes = new Uint8Array([""" + u_int_8_array_string + """]);
-		const blob = new Blob([bytes], {type: 'application/zip'});
-		const a = document.createElement('a');
-		a.href = URL.createObjectURL(blob);
-		a.download = '""" + sanitized_file_name + """';
-		a.click();
-		URL.revokeObjectURL(a.href);
-	""");
+	JavaScriptBridge.eval(JS_FUNCTION_DOWNLOAD_ZIP_FILE.format(u_int_8_array_string, sanitized_file_name));
